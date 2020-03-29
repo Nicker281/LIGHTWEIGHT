0718b672a3615b876c2f2c3f50ef92288700dfea
senne002 <senne.rosiers@live.be>
2020-03-29 23:44:39 +0200

    Added section on the home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,8 +13,8 @@
                 </div>
                 <div class="col intro-text">
                     <h1>Dream big, Set goals</h1>
-                    <p>Get LightWeigt</p>
-                    <a href="#">
+                    <p>Get LightWeight</p>
+                    <a href="#scroll">
                         <h1>
                             Explore <br>
                             <i class="fas fa-angle-double-down"></i>
@@ -24,4 +24,16 @@
             </div>
         </div>
     </header>
+    <section id="scroll">
+        <h1>store</h1>
+        <p>Hier komt een introductie naar de shop, de merch</p>
+    </section>
+    <section style="background-color:black; color:white;">
+        <h1>challenge</h1>
+        <p>Hier komt een introductie naar de challenges, de community</p>
+    </section>
+    <section>
+        <h1>get to know us</h1>
+        <p>hierkomt een korte verwijzing naar de about us</p>
+    </section>
 @endsection
