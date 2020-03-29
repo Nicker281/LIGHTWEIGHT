c665d202dcbff5aba43c4753ea4e16c4dde572f9
senne002 <senne.rosiers@live.be>
2020-03-29 12:20:54 +0200

    Added views & fixed language

---
+++ resources/views/about.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <h1>About</h1>
+</body>
+</html>
