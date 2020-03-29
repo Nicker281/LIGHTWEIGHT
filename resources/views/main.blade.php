3525c7eed6c637653da5becda4d9c561162ca69e
senne002 <senne.rosiers@live.be>
2020-03-29 16:47:53 +0200

    Added nav

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,12 +5,16 @@
     <meta name="viewport" content="width=device-width initial-scale=1.0">
     <title>@yield('title')LightWeight</title>
     
-    <link rel="icon" href="favicon.ico">
+    <link rel="icon" href="favicon.png">
     <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="css/fontawesome.css">
+    <link rel="stylesheet" href="css/main.css">
     @yield('css')
 </head>
 <body>
+    @include('nav')
     @yield('content')
+    @yield('footer')
     
     <script src="js/app.js"></script>
     @yield('js')
---
+++ resources/views/nav.blade.php
@@ -0,0 +1,32 @@
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="/"><img src="img/Logo-white.png" alt="LightWeight logo" height="35px"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link nav-ani" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav-ani" href="/store">Store</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav-ani" href="#">Challenges</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav-ani" href="/about">About</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="navbar-brand" href="https://instagram.com"><i class="fab fa-2x
+                     fa-instagram"></i></a>
+                </li>
+                <li class="nav-item">
+                    <a class="navbar-brand" href="https://tiktok.com"><img src="/img/tiktok.png" alt="TikTok" height="30px"></a>
+                </li>
+            </ul>
+        </div>
+    </nav>
