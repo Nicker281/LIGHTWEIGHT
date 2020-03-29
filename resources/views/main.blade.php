ac68cf7a634891ec38c0938974936f7a60ad9dec
senne002 <senne.rosiers@live.be>
2020-03-29 14:20:32 +0200

    Added css & js (bootstrap)

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -2,12 +2,17 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width initial-scale=1.0">
     <title>@yield('title')LightWeight</title>
     
-    <link rel="stylesheet" href="">
+    <link rel="icon" href="favicon.ico">
+    <link rel="stylesheet" href="css/app.css">
     @yield('css')
 </head>
 <body>
     @yield('content')
+    
+    <script src="js/app.js"></script>
+    @yield('js')
 </body>
 </html>
