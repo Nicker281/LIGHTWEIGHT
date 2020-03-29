09d2c59ebc4d05f22c0851f2a4b2f128f5607344
senne002 <senne.rosiers@live.be>
2020-03-29 21:58:21 +0200

    Addes home.css & start home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,16 @@
 @extends('main')
 
+@section('css')
+    <link rel="stylesheet" href="css/home.css">
+@endsection
+
 @section('content')
-    <h1>Home</h1>
+    <header>
+        <div class="container">
+            <div class="intro">
+                <div class="intro-title">logo</div>
+                <div class="intro-text">tekst</div>
+            </div>
+        </div>
+    </header>
 @endsection
