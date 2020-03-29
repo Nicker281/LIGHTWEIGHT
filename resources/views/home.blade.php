8a43365439718cccc7fcb124e096b58e0b3a6c3b
senne002 <senne.rosiers@live.be>
2020-03-30 00:47:34 +0200

    Added scroll animation when pressing on Explore on home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -37,3 +37,15 @@
         <p>hierkomt een korte verwijzing naar de about us</p>
     </section>
 @endsection
+
+@section('js')
+    <script>
+        $('a[href*="#"]').on('click', function(e) {
+            e.preventDefault()
+            $('html, body').animate(
+                {
+                    scrollTop: $($(this).attr('href')).offset().top,
+                }, 500, 'linear')
+            })
+    </script>
+@endsection
