5a27982b761b5706db39684f5ddfb8e23873e3ae
senne002 <senne.rosiers@live.be>
2020-03-31 13:21:51 +0200

    Added shop section on home for desktop & mobile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                 <div class="col-12 col-lg-6 banner-text">
                     <h1 class="banner-small">Dream big, Set goals</h1>
                     <p>Get LightWeight</p>
-                    <a href="#scroll" class="banner-link">
+                    <a href="#shop" class="banner-link">
                         <h1 class="banner-small">
                             Explore <br>
                             <i class="fas fa-angle-double-down"></i>
@@ -24,10 +24,37 @@
             </div>
         </div>
     </header>
-    <section id="scroll">
-        <h1>store</h1>
-        <p>Hier komt een introductie naar de shop, de merch</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni aliquam eos laboriosam ex velit, minus maxime, ut perspiciatis possimus quaerat quas ipsam, saepe explicabo nobis eveniet doloremque porro mollitia nulla!</p>
+    <section id="shop">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-4"></div>
+                <div class="bgWhite col">
+                    <h1 class="text-center">Visit our shop</h1>
+                    <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut dolorem, dicta harum delectus est aut facilis necessitatibus quasi nobis maxime deserunt voluptate nulla unde voluptatem sed cupiditate iure blanditiis soluta!</p>
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-12 col-md-4"></div>
+                <div class="justify-content-center col-12 col-md-4 col-lg">
+                    <div class="imgC" id="imgC1"></div>
+                </div>
+                <div class="justify-content-center col-12 col-md-4 col-lg">
+                    <div class="imgC" id="imgC2"></div>
+                </div>
+                <div class="justify-content-center col-12 offset-0 col-md-8 offset-md-4 col-lg offset-lg-0">
+                    <div class="imgC" id="imgC3"></div>
+                </div>
+            </div>
+            <br>
+            <br>
+            <div class="row">
+                <div class="col-12 col-md-4"></div>
+                <div class="text-center col">
+                    <a class="btn btn-primary" href="/shop">Go To Shop</a>
+                </div>
+            </div>
+        </div>
     </section>
     <section style="background-color:black; color:white;">
         <h1>challenge</h1>
