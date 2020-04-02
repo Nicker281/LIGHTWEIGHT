e31cbafdc6a2f600d75c05c514a5855ea6b39ee0
senne002 <senne.rosiers@live.be>
2020-04-02 12:48:27 +0200

    Made banner shop page

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -2,6 +2,21 @@
 
 @section('title', 'Store | ')
 
+@section('css')
+    <link rel="stylesheet" href="css/shop.css">
+@endsection
+
 @section('content')
-    <h1>Shop</h1>
+    <header>
+        <div class="container">
+            <div class="row banner">
+                <div class="col">
+                    <a class="btn btn-secondary" href="/shop">Woman</a>
+                </div>
+                <div class="col">
+                    <a class="btn btn-secondary" href="/shop">Men</a>
+                </div>
+            </div>
+        </div>
+    </header>
 @endsection
