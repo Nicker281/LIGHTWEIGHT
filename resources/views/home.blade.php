b05917a116e63d007d84cdebc6b56f2785864656
senne002 <senne.rosiers@live.be>
2020-04-02 13:02:02 +0200

    Made scroll.js and Changed some css from shop page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -101,13 +101,5 @@
 @endsection
 
 @section('js')
-    <script>
-        $('a[href*="#"]').on('click', function(e) {
-            e.preventDefault()
-            $('html, body').animate(
-                {
-                    scrollTop: $($(this).attr('href')).offset().top,
-                }, 500, 'linear')
-            })
-    </script>
+    <script src="js/scroll.js"></script>
 @endsection
--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -17,6 +17,29 @@
                     <a class="btn btn-secondary" href="/shop">Men</a>
                 </div>
             </div>
+            <div class="row banner">
+                <div class="col">
+                    <a href="#shop" class="banner-link">
+                        <h1 class="banner-small">
+                            Explore Products<br>
+                            <i class="fas fa-angle-double-down"></i>
+                        </h1>
+                    </a>
+                </div>
+            </div>
         </div>
     </header>
+    <section id="shop">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h1>Shop under construction</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
+
+@section('js')
+    <script src="js/scroll.js"></script>
 @endsection
