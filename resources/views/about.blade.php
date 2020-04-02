3bbc45a95344b61db18f6d125de2e9f96e1c80d5
senne002 <senne.rosiers@live.be>
2020-04-02 12:20:33 +0200

    Made about page for pc, tablet & mobile

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -10,13 +10,20 @@
     <header>
         <div class="container">
             <div class="row banner">
-                <div class="col banner-text">
-                    <p>- Comming Soon -</p>
-                    <h1>
-                        "The only workout is<br>
-                        the one that did not happen"<br>
-                        ~LightWeight
-                    </h1>
+                <div class="col-12">
+                    <h1>Sannie</h1>
+                    <p>Sannie has been training for 9 years. He found his passion for fitness trough getting bullied as a kid. One day I made the choice of not letting that happen anymore. The gym was and still is my therapy. I love everything about it, the people you meet but also the things you can do that you never thought possible. I love helping people get to their goals. Lightweight for me is the fact that you can get over mental obstacles, because the only limits are the ones you have in your mind.</p>
+                    <h1>Big M</h1>
+                    <p>Big M is the man that has his goal set. He goes the extra mile to fulfill not only his dreams, but also yours. Do not give up until you achieve your goals, go for it. Dare to dream big. I focus on how to train professionally. Since 2018 I am, on a daily basis, working out on how to create the perfect balance between a healthy diet and a great workout schedule.</p>
+                </div>
+                <div class="col-12">
+                    <h1>Goals</h1>
+                </div>
+                <div class="col-12 col-md-6">
+                    <p>What can you expect from Lightweight? Just basically every thing you need regarding your fitness goals. Creating workout challenges, exercises that will bring you that much further. Remarkable improvements will make you wanting to go the extra mile yourself.</p>
+                </div>
+                <div class="col-12 col-md-6">
+                    <p>Lightweight will help you all the way. We are there for you, with you, together we will go beyond your expectations! Dream big, set goals, a healthy and strong body will give you the boost to continue your dream.</p>
                 </div>
             </div>
         </div>
